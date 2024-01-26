e3348f00f9d5d0b696d68959eb508a55879f1e49
Anuradha S <49212192+anuradhases@users.noreply.github.com>
2024-01-26 00:17:02 -0500

    Add files via upload

--- Patients_Analysis_EDA.sql
+++ Patients_Analysis_EDA.sql
@@ -47,10 +47,20 @@ FROM pre_auth_null_counts
 WHERE null_percent > 5
 ORDER BY null_percent DESC;
 
+--30% missing data for patient_id
+
 --since no column with more than 60% missing data, no effect on statistical conclusion
 
 --Change column types?
 --Convert DOB to Date
+
+
+--view distinct values in Gender
+
+SELECT DISTINCT(GENDER)
+FROM pre_auth_patients_cleaned;
+-- value as 0, this data will be ignored, since there is not enough information to impute
+
 
 --Patients with Appts 
 -- View data
@@ -95,16 +105,60 @@ FROM patients_with_appts_null_counts
 WHERE null_percent > 5
 ORDER BY null_percent DESC;
 
+--View distinct values in source
+SELECT DISTINCT(source)
+FROM patients_with_appts_cleaned;
+ --one value as B - do not know source of this patient without more information
 
+ 
 --Exploratory Data Analysis
 
 -- Distinct patient counts (pre_auth patients, direct booking patients, all patients)
--- define pre-auth patients as those in pre_auth_patients dataset and source as pre-auth in patients with appts dataset, no overlap
--- define direct booking patients as those with source = db in patients with appts dataset
--- total patient count
 
---SELECT (SELECT COUNT(DISTINCT(PRE_AUTH_ID))
---FROM pre_auth_patients_cleaned) +  ;
+-- define pre-auth patients as those in pre_auth_patients dataset and source as pre-auth in patients with appts dataset, no overlap
+
+--Will need to merge pre-auth dataset with patients with appts dataset on Patient ID
+
+
+SELECT
+-- pre_auth_patients
+(SELECT COUNT(DISTINCT(pre_auth_id)) FROM pre_auth_patients_cleaned)+
+-- pre_auth patients with source as pre-auth
+(SELECT COUNT(DISTINCT(patient_id)) FROM patients_with_appts_cleaned p_appts
+WHERE NOT EXISTS (SELECT patient_id FROM pre_auth_patients_cleaned pre_auth
+WHERE pre_auth.patient_id = p_appts.patient_id)
+AND source ='pre-auth')
+AS pre_auth_count;
+
+-- 33238
+
+-- define direct booking patients as patients not in pre_auth dataset and source not as 'pre-auth' 
+-- all other sources assumed to be direct booking since there are only two sources where patients can book appointments
+
+SELECT COUNT(DISTINCT(patient_id)) AS direct_booking_count FROM patients_with_appts_cleaned p_appts
+WHERE NOT EXISTS (SELECT patient_id FROM pre_auth_patients_cleaned pre_auth
+WHERE pre_auth.patient_id = p_appts.patient_id)
+AND NOT source = 'pre-auth';
+
+-- 625
+
+-- total patient count
+SELECT
+(SELECT COUNT(DISTINCT (pre_auth_id)) FROM pre_auth_patients_cleaned)+
+(SELECT COUNT(DISTINCT(patient_id)) AS direct_booking_count FROM patients_with_appts_cleaned p_appts
+WHERE NOT EXISTS (SELECT patient_id FROM pre_auth_patients_cleaned pre_auth
+WHERE pre_auth.patient_id = p_appts.patient_id))
+AS total_count;
+
+--33865
+
+
+
+
+
+
+
+
 
 
 
